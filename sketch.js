6d492dbac241d9d23b1a7759888c581631ff4855
kiwi55528 <steven11344200@gmail.com>
2020-07-16 19:43:50 -0700

    added log

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log7
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+log7 = new Log(400,400,92,-PI/2)
 }
 
 function draw(){
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    log7.display();
 }
